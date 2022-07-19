6e11165fa74dd724197857c7bce9c4b70e55b608
JcxaL <jc.li.alvin@gmail.com>
2022-07-19 14:20:56 -0400

    Finished the task list website

--- Day 5/script.js
+++ Day 5/script.js
@@ -3,19 +3,13 @@ class Task{
     constructor(taskName) {
         this.taskName = taskName;
         this.status = false;
+        this.id = taskName.replace(/ /g, "");
     }
     getTaskName(){
         return this.taskName;
     }
-    complete(){
-        this.status = true;
-        return;
-    }
-    incomplete(){
-        this.status = false;
-        return;
-    }
 }
+
 
 const taskInput = document.getElementById("taskInput");
 const addTask = document.getElementById("addTask");
@@ -24,9 +18,6 @@ const table = document.getElementById("tBody");
 const taskCheckBox = document.createElement("div");
 const box = document.getElementById("sampleCheckBox");
 
-// Actions pane 2 buttons: Complete/Incomplete & Delete 
-const completeBtn = document.getElementById("cmpBtn");
-const deleteBtn = document.getElementById("delBtn");
 
 // Text Field and Add Button
 addTask.addEventListener("click", (event) => {
@@ -34,7 +25,7 @@ addTask.addEventListener("click", (event) => {
 
     // Task row holder
     const taskRow = document.createElement("tr");
-    taskRow.id = taskInput.value;
+    taskRow.id = "tr-" + task.id;
 
     // Cell elements
     const taskName = document.createElement("td");
@@ -42,27 +33,37 @@ addTask.addEventListener("click", (event) => {
     taskRow.append(taskName);
 
     const status = document.createElement("td");
-    status.id = "status";
-    status.innerHTML = "";
+    status.id = "s-" + task.id;
+    status.innerHTML = "Incomplete";
     taskRow.append(status);
 
     const actions = document.createElement("td");
-    //delete button
+
+    const cmpBtn = document.createElement("button");
+    cmpBtn.id = "cb-" + task.id;
+    cmpBtn.innerHTML = "Complete";
+    actions.append(cmpBtn);
+
+    cmpBtn.addEventListener("click", (event)=> {
+        // const status = document.getElementById("status");
+        // status.classlist.add();
+        status.innerHTML = "Completed";
+    });
+
+    const delBtn = document.createElement("button");
+    delBtn.id = "db-" + task.id;
+    delBtn.classList.add("bg-dark");
+    delBtn.classList.add("text-light");
+    delBtn.innerHTML = "Delete";
+    actions.append(delBtn);
+
+    delBtn.addEventListener("click", (event) => {
+        // const task = document.find
+        taskRow.remove();
+        return;
+    });
+    
+
     taskRow.append(actions);
-
     table.append(taskRow);
-    console.log(taskRow);
-
-});
-
-// actions column
-completeButton.addEventListener("click", (event)=> {
-    const status = document.getElementById("status");
-    status.classlist.add();
-    status.innerHTML = "Completed";
-});
-                                
-deleteButton.addEventListener("click", (event) => {
-    // const task = document.find
-    table.remove("task");
 });
